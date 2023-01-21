c725fd821c3ceb44ca259a90778c49289bf2c46f
Alparslan Korkmaz <korkmazalparslan.elt@gmail.com>
2023-01-21 12:50:14 +0300

    added gridSize functionality

--- app.js
+++ app.js
@@ -11,5 +11,26 @@ for (i = 0; i < 256; i++) {
 }
 
 button.addEventListener("click", () => {
-  const gridSize = prompt("Enter number of squares per side. (Max 100)");
+  let gridSize = prompt("Enter number of squares per side. (Max 100)");
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.remove();
+  });
+  function createSquares() {
+    for (i = 0; i < gridSize * gridSize; i++) {
+      let squares = document.createElement("div");
+
+      let a = gridSize;
+      const containerSize = 300 / a;
+      squares.classList.add("square");
+      squares.style.width = containerSize + "px";
+      squares.style.height = containerSize + "px";
+      container.appendChild(squares);
+
+      squares.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "orange";
+      });
+    }
+  }
+  createSquares();
 });
