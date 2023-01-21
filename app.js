3e38c16dcd0d76f911831aad9c2ef9d23996b281
Alparslan Korkmaz <korkmazalparslan.elt@gmail.com>
2023-01-21 10:41:25 +0300

    added hovering

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
 for (i = 0; i < 256; i++) {
   const squares = document.createElement("div");
   squares.classList.add("square");
   container.appendChild(squares);
+  squares.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "orange";
+  });
 }
+
+button.addEventListener("click", () => {
+  const gridSize = prompt("Enter number of squares per side. (Max 100)");
+});
